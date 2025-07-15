bfd32564ef70bd354e8d102b1af132150a0160b8
zandrd <amardoneso@icloud.com>
2025-07-15 12:21:26 -0400

    feat: init Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { text, variant = 'default' }: { text: string; variant: 'default' | 'outline' } = $props();
+</script>
+
+<button
+	class={`rounded px-4 py-2 ${variant === 'outline' ? 'border border-neutral-900' : 'bg-neutral-900 text-white'}`}
+>
+	{text}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button text="Boton" variant="default" />
